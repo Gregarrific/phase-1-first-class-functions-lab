d94234dcdfff1ee2c2d3db2c36e0be975c3e707a
Gregory Dutton <greg.dutton@gmail.com>
2023-05-01 08:27:04 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
-
 const returnFirstTwoDrivers = function(drivers) {
     return drivers.slice(0,2);
 }
@@ -8,34 +6,17 @@ const returnLastTwoDrivers = function(drivers) {
     return drivers.slice(-2);
 }
 
-const selectingDrivers = [];
-selectingDrivers[0] = returnFirstTwoDrivers(drivers);
-selectingDrivers[1] = returnLastTwoDrivers(drivers);
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-// console.log(selectingDrivers);
-
-function createFareMultiplier(fareMultiplier, fare) {
-    if (fareMultiplier === 2) {
-        return fare * 2;
-    } else if (fareMultiplier === 3) {
-        return fare* 3;
-    } else if (fareMultiplier === 5) {
-        return fare * 5;
-    } else {
-        return fare = 0;
-    }
+function selectDifferentDrivers(drivers, driverFunction) {
+    return driverFunction(drivers);
 }
 
-function fareDoubler(fare) {
-    return createFareMultiplier(2, fare);
-}
+function createFareMultiplier(fareMultiplier) {
+    return function (fare) {
+      return fareMultiplier * fare;
+    };
+  }
 
-function fareTripler(fare) {
-    return createFareMultiplier(3, fare);
-}
-
-function fareQuintupler(fare) {
-    return createFareMultiplier(5, fare);
-}
-
-// console.log(fareDoubler(10));
+  const fareDoubler = createFareMultiplier(2);
+  const fareTripler = createFareMultiplier(3);
